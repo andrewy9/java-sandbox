5cad9457bea49bdad6bca3d3bb65a746fa4ca870
andrewy9 <andrew.yang993@gmail.com>
2022-11-12 17:26:21 +1300

    Creating and Destroying Objects

--- src/main/java/JavaSandbox.java
+++ src/main/java/JavaSandbox.java
@@ -1,6 +1,49 @@
+import com.andrew.javasandbox.Dog;
+
+import java.util.Optional;
+
 public class JavaSandbox {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to JavaSandbox");
+        Dog oldDog1 = Dog.oldDog();
+        Dog oldDog2 = Dog.oldDog();
+        Dog oldDog3 = new Dog("grandpaDog", 99);
+
+        if (oldDog1 == oldDog2) {
+            System.out.println("1 == 2");
+        }
+
+        if (oldDog1.equals(oldDog2)) {
+            System.out.println("1 eq 2");
+        }
+
+        if (oldDog1 == oldDog3) {
+            System.out.println("1 == 3");
+        }
+
+        if (oldDog1.equals(oldDog3)) {
+            System.out.println("1 eq 3");
+        }
+
+        if (new Dog() == new Dog()) {
+            System.out.println("no1");
+        }
+
+        if (new Dog().equals( new Dog())) {
+            System.out.println("no2");
+        }
+
+        oldDog2.happyDogBirthday();
+
+        if (oldDog1 == oldDog2) {
+            System.out.println("1 == 2");
+        }
+
+        if (oldDog1.equals(oldDog2)) {
+            System.out.println("1 eq 2");
+        }
+
+        oldDog1.typedBark(1);
+        return;
     }
 }
--- src/main/java/com/andrew/javasandbox/Dog.java
+++ src/main/java/com/andrew/javasandbox/Dog.java
@@ -1,15 +1,39 @@
 package com.andrew.javasandbox;
 
+import java.util.ArrayList;
+
 public class Dog {
     private String name;
     private Integer age;
+
+    private static Dog OLD_DOG = new Dog("grandpaDog", 99);
 
     public Dog(String name, Integer age){
         this.name = name;
         this.age = age;
     }
 
+    public Dog(){
+        this.name = "blank";
+        this.age = 1;
+    }
+
+
+    public static Dog oldDog() {
+        return OLD_DOG;
+    }
+
+    public void happyDogBirthday() {
+        this.age ++;
+    }
+
     public void bark () {
         System.out.println("My name is " + this.name + " and I am " + age + "years old");
+    }
+
+    public  <T> ArrayList<T> typedBark(T sound) {
+        ArrayList<T> listValue = new ArrayList<>();
+        listValue.add(sound);
+        return listValue;
     }
 }
